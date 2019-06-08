591738ed58c338ad613a526897bc83e74114aee4
Petr Hofman <hofman.petr@post.cz>
2019-06-09 00:28:50 +0200

    Added Contact model

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    """Contact documentation"""
+    firstname = models.CharField(max_length=254)
+    lastname = models.CharField(max_length=254)
+    # address = https://pypi.org/project/django-address/
+    email = models.EmailField(max_length=254)
+    # phone = https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
+    # skills
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
