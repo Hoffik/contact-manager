0b9054458af7ea4cf68b23cd8f7e37953f4979b1
Petr Hofman <hofman.petr@post.cz>
2019-06-09 14:20:29 +0200

    Added Skill model

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Contact(models.Model):
     """Contact documentation"""
@@ -7,7 +8,15 @@ class Contact(models.Model):
     # address = https://pypi.org/project/django-address/
     email = models.EmailField(max_length=254)
     # phone = https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
-    # skills
 
     def __str__(self):
         return self.firstname + " " + self.lastname
+
+class Skill(models.Model):
+    """Skill documentation"""
+    name = models.CharField(max_length=254) #Add validator for lowercase
+    level = models.PositiveIntegerField(default=1, blank=True, validators=[MinValueValidator(1)])  
+    contacts = models.ManyToManyField(Contact)
+
+    def __str__(self):
+        return self.name
