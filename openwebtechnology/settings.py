93a5b94aa6c28f6ec98a9eb35ca8f61942e4df0f
Petr Hofman <hofman.petr@post.cz>
2019-06-28 01:44:11 +0200

    Repaired Allowed hosts setting

--- openwebtechnology/settings.py
+++ openwebtechnology/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@o*8ti)9uk@^i&1etw=(jeahfawz)(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = [serene-reef-99653.herokuapp.com]
+ALLOWED_HOSTS = ['serene-reef-99653.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
